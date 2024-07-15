3188bb31b8484c288c777796bd283462d57091d5
莊雅竹 <107929195+YaChuuuuu@users.noreply.github.com>
2024-07-15 19:36:34 +0800

    Update fieldexp.R

--- fieldexp.R
+++ fieldexp.R
@@ -45,58 +45,67 @@ library(tidyr)
 
 ggplot(data = FieldExp109live,mapping = aes(x=菌株 , y = 存活率*100, fill = 菌株)) +
   geom_bar(stat = "identity", position = position_dodge(0.8), width = 0.7) + 
-  labs(title="Survival rate of each treatment",y="Survival rate (%)",fill = "Treatment") +
+  labs(title="Survival rate of each treatment",x="",y="Survival rate (%)",fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 ggplot(FieldExp109Grouped_long%>%filter(測量值=="葉綠素"), aes(x = 測量值, y = 平均值, fill = 菌株)) +
   geom_bar(stat = "identity", position = position_dodge(0.8), width = 0.7) +
   geom_errorbar(aes(ymin = 平均值 - 標準差, ymax = 平均值 + 標準差),
                 position = position_dodge(0.8), width = 0.25) +
-  labs(title="Chlorophyll content of each treatment",y = "Chlorophyll content (SPAD)", fill = "Treatment") +
+  labs(title="Chlorophyll content of each treatment",x="",y = "Chlorophyll content (SPAD)", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 ggplot(FieldExp109Grouped_long%>%filter(測量值=="葉面積"), aes(x = 測量值, y = 平均值, fill = 菌株)) +
   geom_bar(stat = "identity", position = position_dodge(0.8), width = 0.7) +
   geom_errorbar(aes(ymin = 平均值 - 標準差, ymax = 平均值 + 標準差),
                 position = position_dodge(0.8), width = 0.25) +
-  labs(title="Leaf area of each treatment",y = "Leaf area (mm^2)", fill = "Treatment") +
+  labs(title="Leaf area of each treatment",x="",y = "Leaf area (mm^2)", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 ggplot(FieldExp109Grouped_long%>%filter(測量值=="冠"), aes(x = 測量值, y = 平均值, fill = 菌株)) +
   geom_bar(stat = "identity", position = position_dodge(0.8), width = 0.7) +
   geom_errorbar(aes(ymin = 平均值 - 標準差, ymax = 平均值 + 標準差),
                 position = position_dodge(0.8), width = 0.25) +
-  labs(title="Crown diameter of each treatment",y = "Crown diamete (mm)", fill = "Treatment") +
+  labs(title="Crown diameter of each treatment",x="",y = "Crown diamete (mm)", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 ggplot(FieldExp109Disease, aes(x=菌株 , y = 病害嚴重度, fill = 菌株)) +
   geom_bar(stat = "identity", position = position_dodge(0.8), width = 0.7) +
-  geom_text(aes(label = 有病的植株數量), position = position_dodge(0.8), vjust = 1)+ 
-  labs(x="Disease severity", fill = "Treatment") +
+  geom_text(aes(label = 有病的植株數量), position = position_dodge(1), vjust = 0)+ 
+  scale_y_continuous(limits =c(0,0.6))+ 
+  labs(title="Disease severity of each treatment",x="",y="Disease severity", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 #繪製boxplot
 ggplot(FieldExp109, aes(x = 菌株, y = 葉綠素, fill = 菌株)) +
   geom_boxplot() +
-  labs(title="Chlorophyll content of each treatment",y = "Chlorophyll content (SPAD)", fill = "Treatment") +
+  labs(title="Chlorophyll content of each treatment",x="",y = "Chlorophyll content (SPAD)", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 ggplot(FieldExp109, aes(x = 菌株, y = 葉面積, fill = 菌株)) +
   geom_boxplot() +
-  labs(title="Leaf area of each treatment",y = "Leaf area (mm^2)", fill = "Treatment") +
+  labs(title="Leaf area of each treatment",x="",y = "Leaf area (mm^2)", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 ggplot(FieldExp109, aes(x = 菌株, y = 冠平均, fill = 菌株)) +
   geom_boxplot() +
-  labs(title="Crown diameter of each treatment",y = "Crown diamete (mm)", fill = "Treatment") +
+  labs(title="Crown diameter of each treatment",x="",y = "Crown diamete (mm)", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 #提取離群值
@@ -123,21 +132,23 @@ glimpse(FieldExp109 %>% filter(葉面積_outliers==1))
 #重新繪圖
 ggplot(FieldExp109 %>% filter(!葉綠素_outliers), aes(x = 菌株, y = 葉綠素, fill = 菌株)) +
   geom_boxplot() +
-  labs(title="Chlorophyll content of each treatment(w/o outlier)",y = "Chlorophyll content (SPAD)", fill = "Treatment") +
+  labs(title="Chlorophyll content of each treatment(w/o outliers)",x="",y = "Chlorophyll content (SPAD)", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 ggplot(FieldExp109 %>% filter(!葉面積_outliers), aes(x = 菌株, y = 葉面積, fill = 菌株)) +
   geom_boxplot() +
-  labs(title="Leaf area of each treatment(w/o outlier)",y = "Leaf area (mm^2)", fill = "Treatment") +
+  labs(title="Leaf area of each treatment(w/o outliers)",x="",y = "Leaf area (mm^2)", fill = "Treatment") +
   theme_minimal() +
   theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 ggplot(FieldExp109 %>% filter(!冠平均_outliers), aes(x = 菌株, y = 冠平均, fill = 菌株)) +
   geom_boxplot() +
-  labs(title="Crown diameter of each treatment(w/o outlier)",y = "Crown diamete (mm)", fill = "Treatment") +
+  labs(title="Crown diameter of each treatment(w/o outliers)",x="",y = "Crown diamete (mm)", fill = "Treatment") +
   theme_minimal() +
+  theme(axis.text.x = element_blank())+
   facet_grid(場域~時間)
 
 #去除outliers檢驗常態
@@ -173,7 +184,4 @@ anova_result_chl <- aov(`葉綠素` ~ 場域 * 時間 * 菌株, data = FieldExp1
 summary(anova_result_chl)
 anova_result_crown <- aov(`冠平均` ~ 場域 * 時間 * 菌株, data = FieldExp109)
 summary(anova_result_crown)
-tukey_result <- TukeyHSD(anova_result_crown, "菌株")
-print(tukey_result)
-plot(tukey_result)
 '''
